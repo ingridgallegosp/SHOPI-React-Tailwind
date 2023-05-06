bda01ac56cef5b8a878e6a0ea3bd2b3435aaa258
Ingrid G <ing.gape18@gmail.com>
2023-05-05 19:26:07 -0500

    clase 6 y 7 consumo de API y estilos en card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,25 @@
-import Layout from "../../Components/Layout"
-
+import { useEffect, useState } from 'react'
+import Layout from '../../Components/Layout'
+import Card from '../../Components/Card'
 const Home = () => {
+
+    const [items, setItems] = useState[null];
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+    }, []);
 
     return (
         <Layout>
             Home
+            { 
+                items?.map(() => {
+                    return <Card/>
+                })
+            }
+            <Card/>
         </Layout> 
   )
 }
