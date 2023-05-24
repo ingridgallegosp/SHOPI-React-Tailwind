f820570214c753c606392e562a1d13a7fd78e195
Ingrid G <ing.gape18@gmail.com>
2023-05-24 12:36:05 -0500

    new date en cards

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -2,12 +2,15 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 
 const OrdersCard = (props) => {
     const { id, totalPrice, totalProducts } = props
+
+    const date = new Date().toDateString()
+    console.log(date)
    
 
     return (
         <div className='flex justify-between items-center mb-3 border border-black'>
             <p>
-                <span>01.02.2023</span>
+                <span>{ date }</span>
                 <span>{ totalPrice }</span>
                 <span>{ totalProducts }</span>
             </p>
